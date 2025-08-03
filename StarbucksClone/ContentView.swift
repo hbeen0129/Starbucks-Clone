b3860fe69d8902d46ee1167b7e9382b6485657a7
HyebeenLee(Emma) <89961823+hbeen0129@users.noreply.github.com>
2025-08-03 16:17:40 +0900

    Initial Commit

---
+++ StarbucksClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarbucksClone
+//
+//  Created by 이혜빈 on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarbucksClone/StarbucksCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksCloneApp.swift
+//  StarbucksClone
+//
+//  Created by 이혜빈 on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
